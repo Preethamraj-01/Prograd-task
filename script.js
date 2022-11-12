2d3eac9c4a8dcd041408a7410137a3092945647a
Preethamraj-01 <preethamraj5734@gmail.com>
2022-11-12 13:02:11 +0530

    Updated

--- script.js
+++ script.js
@@ -70,7 +70,78 @@ var array = [
     author:"rakshitha",
     subject:"thriller",
     pdate:"05-01-2001"
-   }
+   },{
+
+    id:"11",
+    title:"400 days",
+    author:"chetan bhagat",
+    subject:"comedy",
+    pdate:"10-8-2020"
+   },
+   {
+   
+       id:"12",
+       title:"once upon a time",
+       author:"rajesh",
+       subject:"auto-biography",
+       pdate:"13-2-2009"
+      }, {
+   
+       id:"13",
+       title:"friends",
+       author:"preetham",
+       subject:"comedy",
+       pdate:"12-02-2021"
+      }, {
+   
+       id:"14",
+       title:"half girlfriend",
+       author:"chetan bhagat",
+       subject:"love story",
+       pdate:"9-10-2009"
+      }, {
+   
+       id:"15",
+       title:"raazi",
+       author:"zaim ahmed",
+       subject:"comedy",
+       pdate:"10-10-2006"
+      }, {
+   
+       id:"16",
+       title:"a girl in room number 105",
+       author:"chetan bhagat",
+       subject:"suspense",
+       pdate:"7-7-2003"
+      }, {
+   
+       id:"17",
+       title:"murder",
+       author:"suma",
+       subject:"thriller",
+       pdate:"2-12-2005"
+      }, {
+   
+       id:"18",
+       title:"hostel",
+       author:"preetham",
+       subject:"thriller",
+       pdate:"1-1-2001"
+      }, {
+   
+       id:"19",
+       title:"a stranger",
+       author:"chetan bhagat",
+       subject:"suspense",
+       pdate:"12-02-2020"
+      }, {
+   
+       id:"20",
+       title:"bangalore days",
+       author:"rakshitha",
+       subject:"thriller",
+       pdate:"05-01-2001"
+      }
 
 ]
 
@@ -81,7 +152,7 @@ function showtable(curarray){
 document.getElementById("mytable").innerHTML=`
 
 <tr class="bg-primary text-white fw-bold">
-<td>ID</td>
+<td>BOOK_ID</td>
 <td>TITLE</td>
 <td>AUTHOR</td>
 <td>SUBJECT</td>
